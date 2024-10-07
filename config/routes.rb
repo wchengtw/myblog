7606ffabdaebc2b521b13d73b4956537449b522e
wchengtw <mydearwin515@gmail.com>
2024-10-06 20:48:13 -0500

    Fix: Added missing views and migration file

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-resources :posts
 root 'posts#index'
-
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+resources :posts
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
